be980f5bad44693df1ff2ed0c79697716c32b1cf
Renato Westphal <renato@opensourcerouting.org>
2020-05-07 08:32:22 -0300

    lib: support for yang operational-data async iteration
    This commit extends the northbound layer with infrastructure for
    asynchronous YANG-modeled operational data retrieval. This should
    solve the problem of blocking the main pthread of the daemons for
    too long while fetching bulk state data (e.g. millions of routes).
    
    The nb_oper_data_iterate() API function was modified to accept
    a starting offset, a maximum number of elements to iterate over
    and outputs where the iteration stopped. The outputted offset can
    be feeded back to the input to resume the iteration from where
    it stopped. It's up to the NB client whether to fetch and buffer
    all data before delivering it to the external client, or demand
    the external client to be in charge of resuming the iteration
    as necessary. In any case, canceling the iteration before it
    finishes should be possible (small chunk sizes are better for more
    responsiveness).
    
    The challenge with asynchronous iteration is that data can be
    modified while it's being iterated over. In particular, the iteration
    might stop at a given YANG list entry, then once the iteration
    resumes that list entry might not exist anymore. This means the
    pointer returned by the latest get_next() callback is unsafe to use
    once the iteration is resumed. To deal with this problem, the NB
    should use the lookup_entry() callback as a form of synchronization
    to ensure the list entry referent to the base offset of the data
    iteration still exists. Further, a new 'exact_match' parameter was
    added to that callback so that it can find the next available entry
    in the list in case the one we're looking for doesn't exist anymore.
    
    One limitation of the current implementation is that the data
    iteration can't stop in the middle of a leaf-list or keyless
    list. This is because the NB doesn't provide the get_keys() and
    lookup_entry() callbacks for such nodes, so there's no way to perform
    the required synchronization step once the iteration is resumed. In
    the future it should be possible to work around this limitation by
    implementing pseudo-keys that are internal to the FRR implementation
    and not visible to the external world. The ConfD API uses a similar
    strategy to deal with this problem.
    
    As of now, none of the existing NB clients (CLI, gRPC, ConfD and
    Sysrepo) can leverage this new infrastructure since none of them
    have async capabilities. In spite of that, the CLI "show yang
    operational-data" command was extended with the "max-elements
    (1-1000000) [repeat]" parameters for testing purposes (the data
    iteration can be stopped and resumed several times, but the command
    still runs synchronously). These new parameters should be removed
    once the CLI gets async capabilities (in that case a global knob
    to enable fragmentation at a system level should be better).
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- lib/northbound_sysrepo.c
+++ lib/northbound_sysrepo.c
@@ -368,7 +368,7 @@ static int frr_sr_state_data_iter_cb(const struct lys_node *snode,
 
 	listnode_add(elements, data);
 
-	return NB_OK;
+	return NB_ITER_CONTINUE;
 }
 
 /* Callback for state retrieval. */
@@ -377,6 +377,7 @@ static int frr_sr_state_cb(const char *xpath, sr_val_t **values,
 			   const char *original_xpath, void *private_ctx)
 {
 	struct nb_oper_data_iter_input iter_input = {};
+	struct nb_oper_data_iter_output iter_output = {};
 	struct list *elements;
 	struct yang_data *data;
 	struct listnode *node;
@@ -388,7 +389,7 @@ static int frr_sr_state_cb(const char *xpath, sr_val_t **values,
 	iter_input.cb = frr_sr_state_data_iter_cb;
 	iter_input.cb_arg = elements;
 	iter_input.flags = F_NB_OPER_DATA_ITER_NORECURSE;
-	if (nb_oper_data_iterate(&iter_input) != NB_OK) {
+	if (nb_oper_data_iterate(&iter_input, &iter_output) == NB_ITER_ABORT) {
 		flog_warn(EC_LIB_NB_OPERATIONAL_DATA,
 			  "%s: failed to obtain operational data [xpath %s]",
 			  __func__, xpath);
